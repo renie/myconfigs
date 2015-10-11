2f6ba5ca3ee2fdd27f9787271d326578b982aa22
Renie Siqueira <renie.webdev@gmail.com>
2015-10-11 00:28:30 -0300

    Installing Sublime packages.

--- installer.sh
+++ installer.sh
@@ -10,19 +10,29 @@ if [ $script_dir = '.' ]; then
 	script_dir="$current_dir"
 fi
 
+backToScriptDir(){
+	cd "$script_dir"
+}
+
 
 # Sublime installing
 if ! which subl >/dev/null; then
 	cd ~/Downloads
 	wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
 	sudo dpkg -i sublime-text_build-3083_amd64.deb
+	wget https://sublime.wbond.net/Package%20Control.sublime-package
+	cp Package\ Control.sublime-package ~/.config/sublime-text-3/Installed\ Packages
 else
 	echo "Sublime is already installed."
 fi
 
+# Copying Sublime settings
+backToScriptDir
+cp Package\ Control.sublime-settings ~/.config/sublime-text-3/Packages/User/
+cp Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
 
 # Copying bash scripts session
-cd "$script_dir"
+backToScriptDir
 if [ -f ~/.bash_aliases ]; then
 	cp ~/.bash_aliases ~/.bash_aliases__bkp
 fi
@@ -33,4 +43,6 @@ cp .bash_aliases ~
 
 . ~/.bashrc
 
+# Last warnings
+echo "Sublime Text asks for restarting itself for installing everything ¯\_(ツ)_/¯"
 echo "As shellscript runs on another instance of bash, you have to restart your terminal to use new configs."
