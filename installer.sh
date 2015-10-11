be5de501f757f5c15d61734a2fb2ce99973ba1f9
Renie Siqueira <renie.webdev@gmail.com>
2015-10-10 23:39:12 -0300

    Checking, and installing if necessary, Sublime Text.

--- installer.sh
+++ installer.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo "Running installer"
+
+if ! which subl >/dev/null; then
+	cd ~/Downloads
+	wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
+	sudo dpkg -i sublime-text_build-3083_amd64.deb
+else
+	echo "Sublime is already installed."
+fi
+
 if [ -f ~/.bash_aliases ]; then
 	cp ~/.bash_aliases ~/.bash_aliases__bkp
 fi
