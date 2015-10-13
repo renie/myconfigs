c18610caf6d82730f0ea8ae96cd4ac7d3cbfac98
Renie Siqueira <renie.webdev@gmail.com>
2015-10-12 21:16:38 -0300

    Exporting a function for checking existence of a command

--- installer.sh
+++ installer.sh
@@ -2,7 +2,10 @@
 
 echo "Running installer"
 
+
+###
 # Storing script directory
+###
 current_dir=$(pwd)
 script_dir=$(dirname $0)
 
@@ -14,24 +17,37 @@ backToScriptDir(){
 	cd "$script_dir"
 }
 
+verifyCommandExistence(){
+	return $(which $1 >/dev/null)
+}
 
+
+###
 # Sublime installing
-if ! which subl >/dev/null; then
+###
+if verifyCommandExistence subl; then
+	echo "Sublime is already installed."
+else
 	cd ~/Downloads
 	wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
 	sudo dpkg -i sublime-text_build-3083_amd64.deb
 	wget https://sublime.wbond.net/Package%20Control.sublime-package
 	cp Package\ Control.sublime-package ~/.config/sublime-text-3/Installed\ Packages
-else
-	echo "Sublime is already installed."
+	echo "Sublime Text asks for restarting itself for installing everything ¯\_(ツ)_/¯"
 fi
 
+
+###
 # Copying Sublime settings
+###
 backToScriptDir
 cp Package\ Control.sublime-settings ~/.config/sublime-text-3/Packages/User/
 cp Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
 
+
+###
 # Copying bash scripts session
+###
 backToScriptDir
 if [ -f ~/.bash_aliases ]; then
 	cp ~/.bash_aliases ~/.bash_aliases__bkp
@@ -43,6 +59,8 @@ cp .bash_aliases ~
 
 . ~/.bashrc
 
+
+###
 # Last warnings
-echo "Sublime Text asks for restarting itself for installing everything ¯\_(ツ)_/¯"
+###
 echo "As shellscript runs on another instance of bash, you have to restart your terminal to use new configs."
