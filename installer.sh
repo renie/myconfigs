34b8df6b2f04257211ca10854c29e99114ca138a
Renie Siqueira <renie.webdev@gmail.com>
2015-10-13 22:32:05 -0300

    Help implemented. Logs improved.

--- installer.sh
+++ installer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 inittime=$(date +%s)
 
-echo "Running installer"
+
+
+printf "\nInstaller...\n\n\n"
+
 
 
 ###
@@ -23,8 +26,9 @@ verifyCommandExistence(){
 }
 
 
+
 ###
-# Variables
+# Setting variables
 ###
 nodeversion="4.2.0"
 nodearch="64"
@@ -36,28 +40,44 @@ while [ "$1" != "" ]; do
 		--nodearch )   shift
 			nodearch=$1
 		;;
+		-h )   shift
+			printf "Allowed parametes:\n\n"
+			printf "\t--nodeversion\t\tVersion of NodeJS you want. Check available versions on https://nodejs.org/en/download/releases/ \n"
+			printf "\t\t\t\t(io.js not included)\n"
+			printf "\t\t\t\t(default: 4.0.2)\n\n"
 
+			printf "\t--nodearch\t\tArchiteture of NodeJS you want\n"
+			printf "\t\t\t\t(default: 64)\n"
+			printf "\n\n\n"
+			exit 1
+		;;
 		* )
-			echo "Invalid param: $1"
+			printf "Invalid param: $1\n\n\n"
 			exit 1
 	esac
 	shift
 done
 
 
+
+printf "Running installer\n\n"
+
+
 ###
 # Sublime installing
 ###
+printf "Sublime install...\n"
 if verifyCommandExistence subl; then
-	echo "Sublime is already installed."
+	printf "Sublime is already installed.\n"
 else
 	cd ~/Downloads
 	wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
 	sudo dpkg -i sublime-text_build-3083_amd64.deb
 	wget https://sublime.wbond.net/Package%20Control.sublime-package
 	cp Package\ Control.sublime-package ~/.config/sublime-text-3/Installed\ Packages
-	echo "Sublime Text installed."
+	printf "Sublime Text installed.\n"
 fi
+
 
 
 ###
@@ -66,19 +86,21 @@ fi
 backToScriptDir
 cp Package\ Control.sublime-settings ~/.config/sublime-text-3/Packages/User/
 cp Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/
-echo "Sublime asks for restarting itself for installing everything ¯\_(ツ)_/¯."
+printf "Sublime asks for restarting itself for installing everything ¯\_(ツ)_/¯.\n\n"
+
 
 
 ###
 # Node installing
 ###
+printf "NodeJS install...\n"
 nodev=${nodeversion//./""}
 nodefile="node-v$nodeversion-linux-x$nodearch.tar.gz"
 nodepath="node-v$nodeversion-linux-x$nodearch"
 nodeurl="https://nodejs.org/dist/v$nodeversion/$nodefile"
 
 if ! verifyCommandExistence node; then
-	echo "NodeJS is already installed."
+	printf "NodeJS is already installed.\n\n"
 else
 	cd ~/Downloads
 	wget $nodeurl
@@ -87,46 +109,52 @@ else
 	sudo mv /usr/local/$nodepath /usr/local/node_v$nodev
 	sudo ln -s /usr/local/node_v$nodev/bin/node /usr/local/bin/node
 	sudo ln -s /usr/local/node_v$nodev/bin/npm /usr/local/bin/npm
-	echo "NodeJS $nodeversion installed."
+	printf "NodeJS $nodeversion installed.\n\n"
 fi
+
 
 
 ###
 # Grunt and bower installing
 ###
-echo "Installing Grunt."
+printf "Installing Grunt...\n"
 sudo npm install -g grunt-cli
-echo "Grunt installed."
-echo "Installing Bower."
+printf "Grunt installed.\n\n"
+printf "Installing Bower...\n"
 sudo npm install -g bower
-echo "Bower installed."
+printf "Bower installed.\n\n"
+
 
 
 ###
 # Copying bash scripts session
 ###
+printf "Copying aliases and environment configs...\n"
 backToScriptDir
 if [ -f ~/.bash_aliases ]; then
 	cp ~/.bash_aliases ~/.bash_aliases__bkp
 fi
 
 cp ~/.bashrc ~/.bashrc__bkp
-echo "Your bashrc was backed up at ~/.bashrc__bkp ."
+printf "Your bashrc was backed up at ~/.bashrc__bkp .\n"
 cp .bashrc ~
-echo "Your bashrc updated."
+printf "Your bashrc was updated.\n"
 cp .bash_aliases ~
-echo "Your bash_aliases updated."
+printf "Your bash_aliases was updated.\n"
 
 . ~/.bashrc
-echo "Your bashrc executed."
+printf "Your bashrc was executed.\n\n"
 
 finaltime=$(date +%s)
 t=$(($finaltime-$inittime))
 
-echo "Time spent: $t s"
+printf "Time spent: $t s\n\n"
+
 
 
 ###
 # Last warnings
 ###
-echo "As shellscript runs on another instance of bash, you have to restart your terminal to use new configs."
+printf "As shellscript runs on another instance of bash, you have to restart your terminal to use new configs.\n\n"
+printf "Install process finished!!"
+printf "\n\n\n"
